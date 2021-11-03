6b14bd97ce1449ff2a5995ec8c8e1524e07f5239
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-03 19:21:50 -0400

    Python Cases

--- Breast_Cancer.py
+++ Breast_Cancer.py
@@ -2,6 +2,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
-# Importing the dataset
-dataset = pd.read_csv('/Users/jasonfang/Work/Dataset/Practical/creditcard.csv')
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+
+cancer.keys()
+
+print(cancer['DESCR'])
+print(cancer['target'])
+print(cancer['target_names'])
+print(cancer['feature_names'])
+print(cancer['data'].shape)
+
+df_cancer = pd.DataFrame(np.c_[cancer['data'],cancer['target']],columns=np.append(cancer['feature_names'],['target']))
+
+df_cancer.head()
+df_cancer.tail()
+
+#visualizing the data
+
+
+sns.pairplot(df_cancer, hue = 'target',vars = ['mean radius', 'mean texture','mean perimeter', 'mean area',
+ 'mean smoothness'], height=2)
+
+
+
+
+
+
+
