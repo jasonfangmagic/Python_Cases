de748bf74bb41e91a6c33f50c6765b6350a210be
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-03 12:36:37 -0400

    Python Cases

--- Breast_Cancer.py
+++ Breast_Cancer.py
@@ -1,2 +1,7 @@
-import tensorflow
-print("Halo")
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('/Users/jasonfang/Work/Dataset/Practical/creditcard.csv')
