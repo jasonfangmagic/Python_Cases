19ccd99832b1a5d4fe3b1b8888fd507d2c04370a
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-06 23:10:01 -0400

    /

--- Fashion_Case.py
+++ Fashion_Case.py
@@ -55,4 +55,85 @@ for i in np.arange(0, W_grid * L_grid): # create evenly spaces variables
 
 plt.subplots_adjust(hspace=0.4)
 
+#training
+X_train = training[:,1:]/255
+y_train = training[:,0]
+X_test = testing[:,1:]/255
+y_test = testing[:,0]
 
+from sklearn.model_selection import train_test_split
+X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size = 0.2, random_state = 0)
+
+X_train.shape
+# * unpack the tuple
+X_train = X_train.reshape(X_train.shape[0], *(28, 28, 1))
+X_test = X_test.reshape(X_test.shape[0], *(28, 28, 1))
+X_validate = X_validate.reshape(X_validate.shape[0], *(28, 28, 1))
+
+# Import Keras
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
+
+cnn_model = Sequential()
+
+# Try 32 fliters first then 64
+cnn_model.add(Conv2D(64,3, 3, input_shape = (28,28,1), activation='relu'))
+cnn_model.add(MaxPooling2D(pool_size = (2, 2)))
+cnn_model.add(Dropout(0.25))
+
+# cnn_model.add(Conv2D(32,3, 3, activation='relu'))
+# cnn_model.add(MaxPooling2D(pool_size = (2, 2)))
+cnn_model.add(Flatten())
+cnn_model.add(Dense(output_dim = 32, activation = 'relu'))
+cnn_model.add(Dense(output_dim = 10, activation = 'sigmoid'))
+
+cnn_model.compile(loss ='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001),metrics =['accuracy'])
+
+epochs = 50
+
+history = cnn_model.fit(X_train,
+                        y_train,
+                        batch_size = 512,
+                        nb_epoch = epochs,
+                        verbose = 1,
+                        validation_data = (X_validate, y_validate))
+
+evaluation = cnn_model.evaluate(X_test, y_test)
+print('Test Accuracy : {:.3f}'.format(evaluation[1]))
+
+import pickle
+# with open("fashion.pickle", "wb") as f:
+#     pickle.dump(cnn_model, f)
+#
+pickle_in = open("fashion.pickle", "rb")
+cnn_model = pickle.load(pickle_in)
+
+predicted_classes = cnn_model.predict_classes(X_test)
+
+L = 5
+W = 5
+fig, axes = plt.subplots(L, W, figsize = (12,12))
+axes = axes.ravel() #
+
+for i in np.arange(0, L * W):
+    axes[i].imshow(X_test[i].reshape(28,28))
+    axes[i].set_title("Prediction Class = {:0.1f}\n True Class = {:0.1f}".format(predicted_classes[i], y_test[i]))
+    axes[i].axis('off')
+
+plt.subplots_adjust(wspace=0.5)
+
+#cm
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, predicted_classes)
+plt.figure(figsize = (14,10))
+sns.heatmap(cm, annot=True)
+
+from sklearn.metrics import classification_report
+
+num_classes = 10
+target_names = ["Class {}".format(i) for i in range(num_classes)]
+
+print(classification_report(y_test, predicted_classes, target_names = target_names))
