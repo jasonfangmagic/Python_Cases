a4b0c6ed4c4c27727c4e9d9aa995f4c899e5ffc0
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-08 11:45:08 -0500

    /

--- FintechApp.py
+++ FintechApp.py
@@ -33,3 +33,55 @@ dataset2.corrwith(dataset.enrolled).plot.bar(figsize=(20, 10),
                                              title='Correlation with Reposnse variable',
                                              fontsize=15, rot=45,
                                              grid=True)
+
+dataset2.corrwith(dataset.enrolled).plot.bar(figsize=(10,8),
+                  title = 'Correlation with Reposnse variable',
+                  fontsize = 10, rot = 45,
+                  grid = True)
+
+
+
+## Correlation Matrix
+sns.set(style="white", font_scale=2)
+
+# Compute the correlation matrix
+corr = dataset2.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(15, 8))
+f.suptitle("Correlation Matrix", fontsize = 12)
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+print(dataset2.corr())
+
+dataset.dtypes
+
+# Formatting Date Columns
+dataset.dtypes
+dataset["first_open"] = [parser.parse(row_date) for row_date in dataset["first_open"]]
+dataset["enrolled_date"] = [parser.parse(row_date) if isinstance(row_date, str) else row_date for row_date in dataset["enrolled_date"]]
+dataset.dtypes
+
+# Selecting Time For Response
+dataset["difference"] = (dataset.enrolled_date-dataset.first_open).astype('timedelta64[h]')
+response_hist = plt.hist(dataset["difference"].dropna(), color='#3F5D7D')
+plt.title('Distribution of Time-Since-Screen-Reached')
+plt.show()
+
+
+plt.hist(dataset["difference"].dropna(), color='#3F5D7D', range = [0, 100])
+plt.title('Distribution of Time-Since-Screen-Reached')
+plt.show()
+
+dataset.loc[dataset.difference > 48, 'enrolled'] = 0
+dataset = dataset.drop(columns=['enrolled_date', 'difference', 'first_open'])
