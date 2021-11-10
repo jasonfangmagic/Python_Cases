6b8c27239a07b121d1071960d96b07ce1ea11a37
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-10 16:52:18 -0500

    /

---
+++ Fraud_Detection.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import keras
+
+data = pd.read_csv('/Users/jasonfang/Work/Dataset/Practical/creditcard.csv')
