2b9896801c93616d797436b23dc53845cb3faaf2
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-10 10:49:43 -0500

    /

--- Churn_Rate_Analysis.py
+++ Churn_Rate_Analysis.py
@@ -6,6 +6,7 @@ import numpy as np
 import seaborn as sns
 
 dataset = pd.read_csv('P39-Minimizing-Churn-Data/churn_data.csv')
+
 # Users who were 60 days enrolled, churn in the next 30
 
 dataset.head()
---
+++ Loan_Risk_Analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+dataset = pd.read_csv('P39-CS5-Python-Code/P39-Financial-Data.csv')
