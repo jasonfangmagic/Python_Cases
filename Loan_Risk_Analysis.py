1c30decb30a4333bd10509b89da6c3788adb7038
jasonfangmagic <jasonfangmagic@gmail.com>
2021-11-10 16:47:54 -0500

    /

--- Loan_Risk_Analysis.py
+++ Loan_Risk_Analysis.py
@@ -4,3 +4,202 @@ import numpy as np
 import seaborn as sns
 
 dataset = pd.read_csv('P39-CS5-Python-Code/P39-Financial-Data.csv')
+
+dataset.head()
+dataset.describe()
+dataset.isna().any()
+dataset.isna().sum()
+
+dataset2 = dataset.drop(columns=['entry_id', 'pay_schedule', 'e_signed'])
+
+fig = plt.figure(figsize=(15, 8))
+plt.suptitle('Histograms of Numerical Columns', fontsize=20)
+for i in range(dataset2.shape[1]):
+    plt.subplot(6, 3, i + 1)
+    f = plt.gca()
+    f.set_title(dataset2.columns.values[i])
+
+    vals = np.size(dataset2.iloc[:, i].unique())
+    if vals >= 100:
+        vals = 100
+
+    plt.hist(dataset2.iloc[:, i], bins=vals, color='#3F5D7D')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+## Correlation with Response Variable (Note: Models like RF are not linear like these)
+
+dataset2.corrwith(dataset.e_signed).plot.bar(
+    figsize=(20, 6), title="Correlation with E Signed", fontsize=10,
+    rot=45, grid=True)
+
+## Correlation Matrix
+sns.set(style="white")
+
+# Compute the correlation matrix
+corr = dataset2.corr()
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(18, 8))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+           square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+
+# Feature Engineering
+
+dataset = dataset.drop(columns = ['months_employed'])
+dataset['personal_account_months'] = (dataset.personal_account_m + (dataset.personal_account_y * 12))
+dataset[['personal_account_m', 'personal_account_y', 'personal_account_months']].head()
+dataset = dataset.drop(columns = ['personal_account_m', 'personal_account_y'])
+
+# One Hot Encoding
+dataset = pd.get_dummies(dataset)
+dataset.columns
+dataset = dataset.drop(columns = ['pay_schedule_semi-monthly'])
+
+# Removing extra columns
+response = dataset["e_signed"]
+users = dataset['entry_id']
+dataset = dataset.drop(columns = ["e_signed", "entry_id"])
+
+# Splitting into Train and Test Set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(dataset,
+                                                    response,
+                                                    test_size = 0.2,
+                                                    random_state = 0)
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+## Logistic Regression
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0, penalty = 'l2')
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+#ann model
+import tensorflow as tf
+
+ann = tf.keras.models.Sequential()
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+# Adding the second hidden layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+# Adding the output layer
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Compiling the ANN
+ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training the ANN on the Training set
+X_train = np.asarray(X_train)
+y_train = np.asarray(y_train)
+X_test = np.asarray(X_test)
+y_test = np.asarray(y_test)
+
+ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+#svm
+## SVM (Linear)
+from sklearn.svm import SVC
+classifier = SVC(random_state = 0, kernel = 'linear')
+classifier.fit(X_train, y_train)
+
+
+## RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(random_state = 0, n_estimators = 100,
+                                    criterion = 'entropy')
+classifier.fit(X_train, y_train)
+
+## K-fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X= X_train, y = y_train,
+                             cv = 10)
+print("Random Forest Classifier Accuracy: %0.2f (+/- %0.2f)"  % (accuracies.mean(), accuracies.std() * 2))
+
+
+
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+f1 = f1_score(y_test, y_pred)
+print(f1)
+acc=accuracy_score(y_test, y_pred)
+print(acc)
+
+# Predicting Test Set
+y_pred = classifier.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+model_results = pd.DataFrame([['Random Forest', acc, prec, rec, f1]],
+               columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score'])
+
+# Round 1: Entropy
+parameters = {"max_depth": [3, None],
+              "max_features": [1, 5, 10],
+              'min_samples_split': [2, 5, 10],
+              'min_samples_leaf': [1, 5, 10],
+              "bootstrap": [True, False],
+              "criterion": ["entropy"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier, # Make sure classifier points to the RF model
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1)
+
+import time
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
+# Round 2: Entropy
+parameters = {"max_depth": [None],
+              "max_features": [3, 5, 7],
+              'min_samples_split': [8, 10, 12],
+              'min_samples_leaf': [1, 2, 3],
+              "bootstrap": [True],
+              "criterion": ["entropy"]}
+
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(estimator = classifier, # Make sure classifier points to the RF model
+                           param_grid = parameters,
+                           scoring = "accuracy",
+                           cv = 10,
+                           n_jobs = -1)
+
+t0 = time.time()
+grid_search = grid_search.fit(X_train, y_train)
+t1 = time.time()
+print("Took %0.2f seconds" % (t1 - t0))
+
+rf_best_accuracy = grid_search.best_score_
+rf_best_parameters = grid_search.best_params_
+rf_best_accuracy, rf_best_parameters
+
